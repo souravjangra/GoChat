ac297c12d940d953da293c83e9bbc03887b14bba
Sourav Jangra <imsouravjangra@gmail.com>
2019-04-15 01:49:25 +0530

    client added!

---
+++ Client.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Client extends JFrame{
+
+    private JTextField userText;
+    private JTextArea chatWindow;
+    private ObjectOutputStream output;
+    private ObjectInputStream input;
+    private String message = "";
+    private String serverIP;
+    private Socket connection;
+
+    // constructor 
+    public Client(String host){
+        super("Client there!");
+        serverIP = host;
+        
+        userText = new JTextField();
+        userText.setEditable(false);
+        userText.addActionListener(
+            new ActionListener(){
+                public void actionPerformed(ActionEvent event){
+                    sendData(event.getActionCommand());
+                    userText.setText("");
+                }
+            }
+        );
+        add(userText, BorderLayout.NORTH);
+        chatWindow = new JTextArea();
+        add(new JScrollPane(chatWindow),BorderLayout.CENTER);
+        setSize(300,150);
+        setVisible(true);
+    }
+}
